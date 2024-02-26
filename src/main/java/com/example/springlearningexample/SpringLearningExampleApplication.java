738849c447abe484d39de491f0569ad8191aace4
Nacora <bdg407@gmail.com>
2024-02-26 22:46:07 +0900

    first commit

---
+++ src/main/java/com/example/springlearningexample/SpringLearningExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlearningexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearningExampleApplication.class, args);
+    }
+
+}
